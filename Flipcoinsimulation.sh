45eb9cbd2f76f69c76d7323d5ac1283d21fabd11
navyas211@gmail.com <navyas211@gmail.com>
2021-09-02 22:03:05 +0530

    [NAVYA]ADDED: Loop through multiple times of flipping a coin

--- Flipcoinsimulation.sh
+++ Flipcoinsimulation.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
-
-coinflip=$((RANDOM%2))
-IS_HEAD=1
-if [ $coinflip -eq 1 ]
+read -p "Enter no of times to toss: " n
+heads_count=0
+tails_count=0
+for(( i =1; i <= n; i++ ))
+do
+   toss=$((RANDOM % 2 ))
+    if(( toss == 0 ))
+    then
+       echo "Flip-$i Heads"
+        (( heads_count++ ))
+        else
+          echo "Flip-$i Tails"
+           (( tails_count++ ))
+          fi
+done
+echo "The Heads count is $heads_count and Tails count is $tails_count"
+if(( head_count > tails_count ))
 then
-   echo HEAD
+   echo "Winner is Heads"
+elif(( tails_count > heads_count ))
+then
+    echo "Winner is Tails"
 else
-   echo TAIL
+echo "Its Tie"
 fi
